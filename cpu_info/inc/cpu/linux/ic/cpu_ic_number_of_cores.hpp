3fe516f8b931bb569bb1223d8c8f97b3db0a7c75
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-08-26 11:43:52 +0300

    Намечен функционал класса cpu_ic_number_of_cores

--- cpu_info/inc/cpu/linux/ic/cpu_ic_number_of_cores.hpp
+++ cpu_info/inc/cpu/linux/ic/cpu_ic_number_of_cores.hpp
@@ -1,1 +1,35 @@
- 
+#ifndef CPU_IC_NUMBER_OF_CORES_HPP
+#define CPU_IC_NUMBER_OF_CORES_HPP
+
+
+#include "information_collector.hpp"
+#include <cstdint>
+
+
+namespace ic {
+    namespace linux {
+
+         /**
+        * @class   cpu_ic_number_of_cores
+        * @author  Kirill Zalomov
+        * @brief   Класс для получения кол-ва ядер процессора.
+        *          Для ОС с ядром Linux.
+        *
+        * @details Класс возвращает кол-во ядер процессора в виде size_t.
+        *          Для получения кол-ва ядер процессора используются системные
+        *          вызовы ядра Linux.
+        */
+        class cpu_ic_number_of_cores
+        : public ic::information_collector<std::size_t> {
+
+            std::size_t collect() const override;
+
+        };
+
+
+    }
+}
+
+
+#endif // CPU_IC_NUMBER_OF_CORES_HPP
+
