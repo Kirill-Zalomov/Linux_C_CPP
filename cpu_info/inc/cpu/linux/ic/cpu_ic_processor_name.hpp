ce6f5d7fb9fae8d4e11a0603827ad7b4f00dfb07
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-08-26 11:36:45 +0300

    Намечен функционал класса cpu_ic_processor_name

--- cpu_info/inc/cpu/linux/ic/cpu_ic_processor_name.hpp
+++ cpu_info/inc/cpu/linux/ic/cpu_ic_processor_name.hpp
@@ -1,1 +1,34 @@
- 
+#ifndef CPU_IC_PROCESSOR_NAME_HPP
+#define CPU_IC_PROCESSOR_NAME_HPP
+
+
+#include "information_collector.hpp"
+#include <string>
+
+
+namespace ic {
+    namespace linux {
+
+         /**
+        * @class   cpu_ic_processor_name
+        * @author  Kirill Zalomov
+        * @brief   Класс для получения названия процессора.
+        *          Для ОС с ядром Linux.
+        *
+        * @details Класс возвращает название процессора в виде std::string.
+        *          Для получения названия процессора используются системные
+        *          вызовы ядра Linux.
+        */
+        class cpu_ic_processor_name
+        : public ic::information_collector<std::string> {
+
+            std::string collect() const override;
+
+        };
+
+
+    }
+}
+
+
+#endif // CPU_IC_PROCESSOR_NAME_HPP
