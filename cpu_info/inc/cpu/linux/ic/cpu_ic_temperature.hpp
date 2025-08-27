87328eb64e99dd252c0d330e436e94a2cc9daac7
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-08-27 19:35:18 +0300

    Намечен функционал класса cpu_ic_temperature

--- cpu_info/inc/cpu/linux/ic/cpu_ic_temperature.hpp
+++ cpu_info/inc/cpu/linux/ic/cpu_ic_temperature.hpp
@@ -1,1 +1,33 @@
- 
+#ifndef CPU_IC_TEMPERATURE_HPP
+#define CPU_IC_TEMPERATURE_HPP
+
+
+#include "information_collector.hpp"
+
+
+namespace ic {
+    namespace linux {
+
+        /**
+        * @class   cpu_ic_temperature
+        * @author  Kirill Zalomov
+        * @brief   Класс для получения текущей температуры процессора.
+        *          Для ОС с ядром Linux.
+        *
+        * @details Класс возвращает текущую температуру процессора в виде
+        *          double. Для получения температуры процессора
+        *          используются системные вызовы ядра Linux.
+        */
+        class cpu_ic_temperature
+        : public ic::information_collector<double> {
+
+            double collect() const override;
+
+        };
+
+
+    }
+}
+
+
+#endif // CPU_IC_TEMPERATURE_HPP
