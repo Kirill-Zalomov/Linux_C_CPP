9f957f5f022e5ab37010cbccdae716cb528d9426
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-08-27 19:39:18 +0300

    Намечен функционал класса cpu_ic_workload_of_cores

--- cpu_info/inc/cpu/linux/ic/cpu_ic_workload_of_cores.hpp
+++ cpu_info/inc/cpu/linux/ic/cpu_ic_workload_of_cores.hpp
@@ -1,1 +1,34 @@
- 
+#ifndef CPU_IC_WORKLOAD_OF_CORES_HPP
+#define CPU_IC_WORKLOAD_OF_CORES_HPP
+
+
+#include "information_collector.hpp"
+#include <list>
+
+
+namespace ic {
+    namespace linux {
+
+        /**
+        * @class   cpu_ic_workload_of_cores
+        * @author  Kirill Zalomov
+        * @brief   Класс для получения текущей загрузки каждого из ядер
+        *          процессора. Для ОС с ядром Linux.
+        *
+        * @details Класс возвращает текущую текущую загрузку ядер процессора
+        *          в виде std::list<double>. Для получения загрузки процессора
+        *          используются системные вызовы ядра Linux.
+        */
+        class cpu_ic_workload_of_cores
+        : public ic::information_collector<std::list<double>> {
+
+            std::list<double> collect() const override;
+
+        };
+
+
+    }
+}
+
+
+#endif // CPU_IC_WORKLOAD_OF_CORES_HPP
